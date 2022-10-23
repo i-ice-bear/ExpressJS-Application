afddbbe22654b7fd3edee15a80089d309eb5609a
ice-bear <andydowsen47us@gmail.com>
2022-10-23 21:21:13 +0530

    Videography Static page in pages/static

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Express")
+const express = require("express")
+const path = require("path")
+
+
+const app = express();
+const port = 3000
+
+app.get("/", (req, res)=>{
+    res.status = 200;
+    res.sendFile(path.join(__dirname, "pages/static/index.html"))
+})
+
+app.listen(port, ()=>{
+    console.log(`App is running on port : ${port}`)
+})
+
+app.use(express.static("pages/static"))
