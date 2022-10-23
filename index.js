0f2c7ebcf07355f3e1b3cc076f5a4116df816c9a
ice-bear <andydowsen47us@gmail.com>
2022-10-23 21:44:23 +0530

    Sample page addition

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const express = require("express")
 const path = require("path")
 
+const myLogger = function (req, res, next) {
+    console.log('LOGGED')
+    next()
+}
+console.log(myLogger)
 
 const app = express();
 const port = 3000
 
+// app.get("/", (req, res)=>{
+//     res.status = 200;
+//     res.sendFile(path.join(__dirname, "pages/static/index.html"))
+// })
 app.get("/", (req, res)=>{
     res.status = 200;
-    res.sendFile(path.join(__dirname, "pages/static/index.html"))
+    res.sendFile(path.join(__dirname, "pages/sample/index.html"))
 })
 
 app.listen(port, ()=>{
     console.log(`App is running on port : ${port}`)
 })
 
-app.use(express.static("pages/static"))
+app.use(express.static("public"))
